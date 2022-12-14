45229fc0f933257088bee2e592f4e4b914a6f7c2
bbes <branimir.bes@gmail.com>
2022-12-14 22:24:54 +0100

    init4

--- index.js
+++ index.js
@@ -1,34 +1,28 @@
 let httpProxy = require('http-proxy');
+//let http = require('http')
 
-let proxy = new httpProxy.createProxyServer({
+let proxy = httpProxy.createServer({
   target: {
     host: 'localhost',
     port: 4075,
-    secure: false
-  }
-});
+  },
+  secure: false,
+})
 
 proxy.on('proxyReq', function(proxyReq, req, res, options) {
-  let reqHost = proxyReq.headers.get("Host")
-  reqHost.replace("bosocp.xyz", "apps-crc.testing")
+  let reqHost = req.headers.host
+  reqHost = reqHost.replace("bosocp.xyz", "apps-crc.testing")
   proxyReq.setHeader('Host', reqHost);
 
-  let reqReferer = proxyReq.headers.get("Referer")
-  if (reqReferer && reqReferer.contains("bosocp.xyz")) {
-    reqHost.replace("bosocp.xyz", "apps-crc.testing")
-  }
+  let reqReferer = req.headers.referer
+  reqReferer = reqReferer.replace("bosocp.xyz", "apps-crc.testing")
+  proxyReq.setHeader('Referer', reqReferer);
+});
 
-  res.oldWriteHead = res.writeHead;
-  res.writeHead = function(statusCode, headers) {
-    let resLocation = res.getHeader('location');
-    if (resLocation && typeof resLocation === "string" && resLocation.includes("apps-crc.testing")) {
-      resLocation.replace("apps-crc.testing", "bosocp.xyz")
-      res.setHeader('location', resLocation);
-    }
-
-    res.oldWriteHead(statusCode, headers);
-  }
-
+proxy.on('proxyRes', function (proxyRes, req, res) {
+  let locationHeader = proxyRes.headers['location']
+  locationHeader = locationHeader.replace("apps-crc.testing", "bosocp.xyz")
+  proxyRes.headers['location'] = locationHeader;
 });
 
 proxy.on('upgrade', function (req, socket, head) {
@@ -37,3 +31,12 @@ proxy.on('upgrade', function (req, socket, head) {
  
 console.log("listening on port 8888")
 proxy.listen(8888);
+
+/*
+http.createServer(function (req, res) {
+  res.setHeader("Location", "apps-crc.testing")
+  res.write('Hello World!'); //write a response to the client
+  //res.setHeader("Location", "apps-crc.testing")
+  res.end(); //end the response
+}).listen(4075); //the server object listens on port 8080
+*/
