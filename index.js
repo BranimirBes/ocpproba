dca39423ae8fb808fd95b1cdd1b447c301f38938
bbes <branimir.bes@gmail.com>
2022-12-14 20:36:15 +0100

    init3

--- index.js
+++ index.js
@@ -9,7 +9,26 @@ let proxy = new httpProxy.createProxyServer({
 });
 
 proxy.on('proxyReq', function(proxyReq, req, res, options) {
-  proxyReq.setHeader('Host', 'console-openshift-console.apps-crc.testing');
+  let reqHost = proxyReq.headers.get("Host")
+  reqHost.replace("bosocp.xyz", "apps-crc.testing")
+  proxyReq.setHeader('Host', reqHost);
+
+  let reqReferer = proxyReq.headers.get("Referer")
+  if (reqReferer && reqReferer.contains("bosocp.xyz")) {
+    reqHost.replace("bosocp.xyz", "apps-crc.testing")
+  }
+
+  res.oldWriteHead = res.writeHead;
+  res.writeHead = function(statusCode, headers) {
+    let resLocation = res.getHeader('location');
+    if (resLocation && typeof resLocation === "string" && resLocation.includes("apps-crc.testing")) {
+      resLocation.replace("apps-crc.testing", "bosocp.xyz")
+      res.setHeader('location', resLocation);
+    }
+
+    res.oldWriteHead(statusCode, headers);
+  }
+
 });
 
 proxy.on('upgrade', function (req, socket, head) {
